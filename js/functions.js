f5b5cfa5405b93da064d68b535cc83e11c6deddc
Rulo180 <rullencio180@gmail.com>
2018-03-07 16:22:59 -0300

    Feature Footer - Create .footer block with content, section and link elements Minor fixes and visual changes to wrap up

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,30 @@
 $(window).scroll(() => {
 
-    let wScroll = $(this).scrollTop();
+    let wScroll = $(this).scrollTop(),
+        pContainerHeight = $('.header-box').height();
 
-    $('.logo').css({
-        'transform': `translateY(${wScroll / 2}%)`
-    });
+    if (wScroll <= pContainerHeight) {
+        $('.logo').css({
+            'transform': `translateY(${wScroll / 2}%)`
+        });
+    
+        $('.back-bird').css({
+            'transform': `translateY(${wScroll / 4}%)`
+        });
+    
+        $('.foreground-bird').css({
+            'transform': `translateY(${- wScroll * 0.005}%)`
+        });
+    }
 
-    $('.back-bird').css({
-        'transform': `translateY(${wScroll / 4}%)`
-    });
-
-    $('.foreground-bird').css({
-        'transform': `translateY(${- wScroll * 0.02}%)`
-    });
+    
 
     if (wScroll > $('.clothes').offset().top - ($(window).height() / 1.2)) {
 
         $('.clothes__figure').each((index) => {
             setTimeout(() => {
                 $('.clothes__figure').eq(index).addClass('clothes__figure--is-showing');
-            }, 150 * (index + 1));
+            }, (700 * (Math.exp(index * 0.14))) - 700);
         });
     }
 
