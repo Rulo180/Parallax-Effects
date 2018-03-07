ad4a246dbe6f9550d778e9535d4cfc11f9c58e78
Rulo180 <rullencio180@gmail.com>
2018-03-07 13:19:28 -0300

    Feature Circle Window - Add promo text  with fade-in animations when scrolling

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,6 @@
 $(window).scroll(() => {
 
     let wScroll = $(this).scrollTop();
-    console.log(wScroll);
 
     $('.logo').css({
         'transform': `translateY(${wScroll / 2}%)`
@@ -23,4 +22,20 @@ $(window).scroll(() => {
             }, 150 * (index + 1));
         });
     }
+
+    let distFromTop = $('.circle-window').offset().top,
+        windowHeight = $(window).height();
+    if (wScroll > distFromTop - windowHeight) {
+        console.log("Hy");
+        $('.circle-window').css({
+            'background-position': `center ${wScroll - distFromTop}px`
+        });
+
+        let cwtOpacity = (wScroll - distFromTop + 400)/ (wScroll/5);
+
+        $('.circle-window__tint').css({
+            'opacity': cwtOpacity
+        });
+    }
+
 });
